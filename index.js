3d9bd5bc4294ff9f42c1f473df5077a562171cdb
Md. Shahrier Hossain Biddut <shahrierhossainbiddut@gmail.com>
2025-06-19 18:19:38 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create a new repository on GitHub
+// Configure git with local folder
+// Create a new README.md and index.js files
+// Added some content to the README.md file
+// Commit the changes to the repository
+// Push the changes to the remote repository
+
+/**
+ * Steps to complete a specific task:
+ * 1. Give instruction to the computer what to do
+ * 2. Process the instruction
+ * 3. Give response to the user
+ */
